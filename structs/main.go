d293c4f4220792b99df767bcbfee3af112885c09
Dhinesh Susaimanickam <dhinesh.susaimanickam@co-operativebank.co.uk>
2020-11-19 00:37:31 +0000

    structs embened into existing

--- structs/main.go
+++ structs/main.go
@@ -7,15 +7,34 @@ import (
 type person struct {
 	firstName string
 	lastName string
+
+	contact contactInfo
+}
+
+type contactInfo struct {
+	email string
+	zip int
 }
 
 func main() {
-	// alex := person {firstName: "Dewin", lastName: "Dhinesh"}
+//First way of declaring the person
+	alex := person {
+		firstName: "Dewin", 
+		lastName: "Dhinesh",
+		contact : contactInfo {
+			email: "dewindhinesh@gmail.com",
+			zip: 638503,
+		},
+	}
 
-	var alex person
+// //another way of declaring the person
+	// var alex person
 
-	alex.firstName = "Dewiz"
-	alex.lastName = "Dhinesh"
+	// alex.firstName = "Dewiz"
+	// alex.lastName = "Dhinesh"
+
+	// alex.contact.email = "dewizdhinesh@gmail.com"
+	// alex.contact.zip = 638503
 
 	// %+v will print verbose
 	fmt.Printf("%+v", alex)

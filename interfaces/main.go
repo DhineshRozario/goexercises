74f30c594f1b16363a7180d6ebf8c3a1bfb9a11d
Dhinesh Susaimanickam <dhinesh.susaimanickam@co-operativebank.co.uk>
2020-11-20 00:18:08 +0000

    added interface to solve the issue

--- interfaces/main.go
+++ interfaces/main.go
@@ -2,29 +2,37 @@ package main
 
 import "fmt"
 
+type bot interface {
+	getGreeting() string
+}
 
-type englishBot struct {}
-
-type spanishBot struct {}
-
-
+type englishBot struct{}
+type spanishBot struct{}
 
 func main() {
+	eb := englishBot{}
+	sb := spanishBot{}
 
+	printGreeting(eb)
+	printGreeting(sb)
+}
+
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
 }
 
 func (eb englishBot) getGreeting() string {
-    return "Hello there!"
+	return "Hello there!"
 }
 
-func (sp spanishBot) getGreeting() string {
-    return "Hola!"
+func (sb spanishBot) getGreeting() string {
+	return "Hola!"
 }
 
-func printGreeting(eb englishBot) {
-    fmt.Println(eb.getGreeting())
-}
+// func printGreeting(eb englishBot) {
+//     fmt.Println(eb.getGreeting())
+// }
 
-func printGreeting(sp spanishBot) {
-    fmt.Println(sp.getGreeting())
-}
+// func printGreeting(sb spanishBot) {
+//     fmt.Println(sb.getGreeting())
+// }

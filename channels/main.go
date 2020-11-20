38ee6fdfaef2eab6c33ac5a6a026c91a87f2a987
Dhinesh Susaimanickam <dhinesh.susaimanickam@co-operativebank.co.uk>
2020-11-20 16:34:27 +0000

    creating channel gets response from only one child

--- channels/main.go
+++ channels/main.go
@@ -15,22 +15,30 @@ func main() {
 		"http://amazon.com",
 	}
 
+	// To sove this problem, 'go' has 'Channel', which acts as a bridge between different 'go' Routines (all child and main routines)
+    // Channel, is type specific, if we create a channel for 'string' msg communication, then we cannot use 'int' to communicate.
+
+    //Creating a channel
+    c := make(chan string)
+
 	//go Routines - have to 'go' keyword only infront of the function.
 	for _, link := range links {
 		fmt.Println("Verfiying the link:", link)
 		//If we add this 'go' keyword, main Routine will create child Routines and exits the program.
-		go checkLink(link)
-	}
-
+		go checkLink(link, c) // added channel c, to communicate with main routine
+    }
+    
+    fmt.Println("Response:", <- c)
 
 	fmt.Println("Main Routine Exits")
 }
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 
 	res, err := http.Get(link)
 
 	if err != nil {
-		fmt.Println("Error: Link might be down:", link, err)
+        fmt.Println("Error: Link might be down:", link, err)
+        c <- link + " Might be down, I think"
 		return
 	}
 
@@ -41,6 +49,6 @@ func checkLink(link string) {
 		}
 		fmt.Println("Response:", link, res.StatusCode, status)
 	}
-
+    c <- link + " Yep, its up"
 	fmt.Println("Chile Routine Exits for link:", link)
 }
